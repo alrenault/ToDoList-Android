163c9a7cb6d04dcff05b023ab352d5b44acc82aa
lois M <monet.lois@hotmail.fr>
2019-01-04 11:54:33 +0100

    Alarm
    more possibilities

--- app/src/main/java/com/todolist/aladdalo/todolist/ToDoNotif.java
+++ app/src/main/java/com/todolist/aladdalo/todolist/ToDoNotif.java
@@ -14,6 +14,12 @@ import android.os.Vibrator;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.orm.query.Condition;
+import com.orm.query.Select;
+import com.todolist.aladdalo.todolist.db.Task;
+
+import java.util.List;
+
 import static android.content.Context.ALARM_SERVICE;
 import static android.content.Context.NOTIFICATION_SERVICE;
 
@@ -22,7 +28,7 @@ public class ToDoNotif {
 
 
 
-    public ToDoNotif(int state,Context context, String taskName, String taskDate){
+    public ToDoNotif(int state,Context context, String taskName, String taskDate,int idTask){
         String stateStr="";
         String commentStr="";
         if(state==0){
@@ -57,6 +63,8 @@ public class ToDoNotif {
             Log.d("Todo_"+this.toString(),"ToDo Alarm:finish");
             builder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM));
 
+            alarmdesactivated(idTask);
+
         }else if(state==0){
             Log.d("Todo_"+this.toString(),"ToDo Alarm:start");
             builder.setDefaults(Notification.DEFAULT_SOUND);
@@ -65,5 +73,20 @@ public class ToDoNotif {
         }
 
         notificationManager.notify((int)System.currentTimeMillis(), builder.build());
+
+
     }
+
+    //Désactive l'alarme de la tache une fois que la deadline est arrivée
+    public void alarmdesactivated(int taskID){
+
+        Task task = Task.findById(Task.class, taskID);
+
+
+        task.setAlarme(false);
+        task.save();
+
+        Log.d("Todo_"+this.toString(),taskID+":desacAlarme|"+task.getAlarme());
+    }
+
 }
